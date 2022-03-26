e5e1da4836b75e1061f298cdf1f674ad64bafed8
Iskander <yangurazov2002@gmail.com>
2022-03-26 18:10:02 +0500

    end 1st step

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -5,19 +5,23 @@
 #include <GL/freeglut.h>
 
 void RenderSceneCB() {
-    glClear(GL_COLOR_BUFFER_BIT);
-    glutSwapBuffers();
+    glClear(GL_COLOR_BUFFER_BIT); //очистка буфера кадра
+    glutSwapBuffers(); //меняем фоновый буфер и буфер кадра местами
 }
 
 int main(int argc, char** argv)
 {
     glutInit(&argc, argv); //инициализация glut
 
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA); //двойная буферизация и буфер цвета
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA); //настройка опций двойная буферизация и буфер цвета
 
     glutInitWindowSize(1024, 768);
     glutInitWindowPosition(100, 100);
     glutCreateWindow("Tutorial 01");
 
     glutDisplayFunc(RenderSceneCB); //очистка буфера
+
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glutMainLoop();
+    return 0;
 }
