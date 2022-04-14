121b6f53e9be8ecaa13aee7f5501e4871f5b212c
Iskander <yangurazov2002@gmail.com>
2022-04-15 00:14:27 +0500

    Пространство камеры

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -68,6 +68,44 @@ glm::mat4 InitPerspectiveProj(float fovy, float aspect, float znear, float zfar)
     return m;
 }
 
+glm::vec3 Cross(const glm::vec3 v1, const glm::vec3 v2) {
+    const float _x = v1.y * v2.z - v1.z * v2.y;
+    const float _y = v1.z * v2.x - v1.x * v2.z;
+    const float _z = v1.x * v2.y - v1.y * v2.x;
+
+    return glm::vec3(_x, _y, _z);
+}
+
+glm::vec3 Normalize(glm::vec3 v)
+{
+    const float Length = sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
+
+    v.x /= Length;
+    v.y /= Length;
+    v.z /= Length;
+
+    return glm::vec3(v.x, v.y, v.z);
+}
+
+glm::mat4 InitCameraTransform(const glm::vec3& Target, const glm::vec3& Up)
+{
+    glm::mat4 m;
+
+    glm::vec3 N = Target;
+    N = Normalize(N);
+    glm::vec3 U = Up;
+    U = Normalize(U);
+    U = Cross(U, Target);
+    glm::vec3 V = Cross(N, U);
+
+    m[0][0] = U.x; m[0][1] = U.y; m[0][2] = U.z; m[0][3] = 0.0f;
+    m[1][0] = V.x; m[1][1] = V.y; m[1][2] = V.z; m[1][3] = 0.0f;
+    m[2][0] = N.x; m[2][1] = N.y; m[2][2] = N.z; m[2][3] = 0.0f;
+    m[3][0] = 0.0f; m[3][1] = 0.0f; m[3][2] = 0.0f; m[3][3] = 1.0f;
+    
+    return m;
+}
+
 void RenderSceneCB() {
     glClear(GL_COLOR_BUFFER_BIT); //очистка буфера кадра
 
@@ -84,10 +122,17 @@ void RenderSceneCB() {
     //glm::mat4 proj = glm::perspective(10.0f, (float)width / (float)height, 1.0f, 100.0f);
     glm::mat4 proj = InitPerspectiveProj(120.0f, (float)width / (float)height, 1.0f, 2000.0f);
 
+    glm::vec3 CameraPos = glm::vec3(1.0f, 1.0f, -3.0f);
+    glm::vec3 CameraTarget = glm::vec3(0.45f, 0.0f, 1.0f);
+    glm::vec3 CameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
 
-    Vertices[0] = glm::vec4(Vertices[0], 1.0f) * proj * mtmove * mtrotation * mtconv;
-    Vertices[1] = glm::vec4(Vertices[1], 1.0f) * proj * mtmove * mtrotation * mtconv;
-    Vertices[2] = glm::vec4(Vertices[2], 1.0f) * proj * mtmove * mtrotation * mtconv;
+    CameraPos = Cross(CameraPos, CameraUp);
+    CameraUp = Normalize(CameraUp);
+    glm::mat4 mtcam = InitCameraTransform(CameraTarget, CameraUp);
+
+    Vertices[0] = glm::vec4(Vertices[0], 1.0f) * proj * mtcam * mtmove * mtrotation * mtconv;
+    Vertices[1] = glm::vec4(Vertices[1], 1.0f) * proj * mtcam * mtmove * mtrotation * mtconv;
+    Vertices[2] = glm::vec4(Vertices[2], 1.0f) * proj * mtcam * mtmove * mtrotation * mtconv;
 
     glGenBuffers(1, &VBO); //генерация объектов переменных типов(кол-во, ссылка на массив)
     glBindBuffer(GL_ARRAY_BUFFER, VBO); //привязываем указатель к названию цели и затем запускаем команду на цель(массив вершин, )
@@ -121,7 +166,7 @@ int main(int argc, char** argv)
     {
         fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
         return 1;
-    }
+    }    
 
     glutMainLoop();
 
